9ac5a1c8980674f15cd0328058977dd580c2992b
anujsinghwd <anuj_singh@outlook.in>
2019-11-30 15:58:06 +0530

    chat app

---
+++ server.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    
+    // Default UserName
+    socket.username = 'Anonymous';
+
+    // UserName Change Req.
+    socket.on('change_username', (data) => socket.username = data.username);
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', {message: msg, username: socket.username});
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(6001, function(){
+    console.log('listening on *:6001');
+});
